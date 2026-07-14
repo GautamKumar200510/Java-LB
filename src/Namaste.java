71d5bc0575547c5dc5cdb3526cd03ae7205f3fde
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-07-14 19:12:19 +0530

    Day1

---
+++ src/Namaste.java
@@ -0,0 +1,9 @@
+public class Namaste {
+    public static void main(String[] args) {
+        System.out.println("Namaste Programmer " + "Not Hello World! ");
+        System.out.println("Java By Love Babbar......");
+        
+        
+    }
+
+}
