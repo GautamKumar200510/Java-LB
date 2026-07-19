775ce5d17c77e6890260e209a3765232f2cc335a
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-07-19 16:24:58 +0530

    Varaible

---
+++ src/Variable/Variable.java
@@ -0,0 +1,19 @@
+public class Variable {
+    public static void main(String[] args) {
+    // String Variable
+        String Name = "Gautam";
+    // Integer Variable
+        int age = 20;
+    // Double Varaible 
+         double Salary= 50000.5;
+
+    // Displaying value of variable
+       System.out.println("Name : " + Name);
+       System.out.println("Age : " + age);
+       System.out.println("Salary : " + Salary);
+
+        
+    }
+
+
+}
