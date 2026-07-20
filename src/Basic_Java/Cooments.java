ecc376caa1829f935bb8b918e7181bfa194b7c25
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-07-20 23:21:03 +0530

    Comments

---
+++ src/Basic_Java/Cooments.java
@@ -0,0 +1,18 @@
+public class Cooments {
+    public static void main(String[] args) {
+// 1. Single-Line Comment (//)
+
+     // Printing the comments
+      System.out.println("Learning the comments");
+    
+
+// Multi-Line Comment (/* ... */)
+
+/*
+This line 
+print multiline comment
+ */
+System.out.println("This is multi comments");
+
+}
+}
