308d50fd857a8a75b5bfb6d840827aa9e29f8ca6
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-07-22 20:24:49 +0530

    Initial commit

---
+++ src/DataType/DataTypes.java
@@ -0,0 +1,34 @@
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        byte num1 = 127;
+        System.out.println(num1);
+
+        short num2 = 3862;
+        System.out.println(num2);
+
+        int num3 = 556;
+        System.out.println(num3);
+
+        long num4 = 972965677;
+        System.out.println(num4);
+
+        float num5 = 3.1424f;
+        System.out.println(num5);
+
+        double num6 = 9.266;
+        System.out.println(num6); 
+
+        boolean vote = true;
+        System.out.println("eligible to vote");
+
+        char firstcharacter = 'a';
+        System.out.println("My first character is:" + firstcharacter);
+
+
+
+
+    }
+
+}
