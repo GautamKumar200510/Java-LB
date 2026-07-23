014e61af9ed9e3a1f33d06245443afb17bcff92c
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-07-23 16:46:52 +0530

    Operator

---
+++ src/Operator/Assignment_Operator.java
@@ -0,0 +1,33 @@
+public class Assignment_Operator {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+
+        a+=5;
+        System.err.println(a);
+
+        a-=3;
+        System.out.println(a);
+
+        a*=2;
+        System.out.println(a);
+
+        a/=2;
+        System.out.println(a);
+
+        a%=3;
+        System.out.println(a);
+
+        a&=2;
+        System.out.println(a);
+
+        a|=2;
+        System.out.println(a);
+
+        a^=3;
+        System.out.println(a);
+
+        
+    }
+
+}
